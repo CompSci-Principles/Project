8677dcca7c85fd6bf2ebc1e5e1c3e4723d0fef41
stock1218 <johnsamuels1218@gmail.com>
2017-03-28 12:04:38 +0100

    Update main.js
    added some stuff

--- Game File/main.js
+++ Game File/main.js
@@ -1,1 +1,21 @@
 
+function setup() {
+    
+    function PCS {
+/**
+This class gives the player class the information about how many points and how much health the player has
+**/
+
+        this.time = 0;
+        this.health = 3;
+    
+}
+   var p = new PCS();
+    print(p.time());
+    
+    
+}
+
+function draw() {
+    
+}
