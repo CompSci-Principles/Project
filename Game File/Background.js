082807d23a7d72db407e8ac1b923a4acdf827d92
stock1218 <johnsamuels1218@gmail.com>
2017-03-30 21:07:52 +0100

    Added functionality
    should be working prototype

--- Game File/Background.js
+++ Game File/Background.js
@@ -1,3 +1,33 @@
 function Background() {
 
+    //RGB Values for the background
+    this.rValue = 0;
+    this.gValue = 0;
+    this.bValue = 0;
+    
+    //Update values for Background
+    this.tick() = function() {
+        rValue += 3;
+        gValue += 2;
+        bValue += 1;
+    };
+    
+    //Getters for the RGB values
+    this.getR = function() {
+     
+        return rValue;
+        
+    };
+    
+       this.getG = function() {
+     
+        return gValue;
+        
+    };
+       
+    this.getB = function() {
+     
+        return bValue;
+        
+    };
 }
