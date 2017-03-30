79bd4555eca02574e4fcc450503d38fdfd977a42
stock1218 <johnsamuels1218@gmail.com>
2017-03-30 11:50:45 +0100

    Added Skeleton

--- Game File/main.js
+++ Game File/main.js
@@ -1,21 +1,11 @@
-
 function setup() {
     
-    function PCS {
-/**
-This class gives the player class the information about how many points and how much health the player has
-**/
-
-        this.time = 0;
-        this.health = 3;
-    
-}
-   var p = new PCS();
-    print(p.time());
     
     
 }
 
 function draw() {
+    
+    
     
 }
