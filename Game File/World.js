27d80fd8d4394f6ea8ebd212eca8534d045fc556
stock1218 <johnsamuels1218@gmail.com>
2017-03-30 21:06:37 +0100

    Added methods
    tick() and isOn()

--- Game File/World.js
+++ Game File/World.js
@@ -1,3 +1,16 @@
 function World() {
     
+    
+    //Update values for the World
+    this.tick = function() {
+        
+    };
+    
+    //We will need to see if an object is on one of the platforms
+    this.isOn = function(x, y) {
+      
+        
+        
+    };
+    
 }
