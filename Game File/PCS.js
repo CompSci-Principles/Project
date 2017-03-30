9e8dc547652e8586871921c8d614c1ca0f40bb26
dpowell6 <dpowell2@stu.acs-schools.com>
2017-03-30 14:23:18 +0100

    Added PCS file

---
+++ Game File/PCS.js
@@ -0,0 +1,16 @@
+function PCS() {
+	//We need our variables
+	
+	this.health = 3;
+
+	
+	//We want to make gitters for health and score
+	this.getHealth = function() {
+	
+		
+		return this.health; 
+		
+		
+	};
+
+}
